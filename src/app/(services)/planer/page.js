681e4ddb888bb3bb0dd15011adf54f7540ca5710
sh20raj <shaswatraj3@gmail.com>
2024-04-17 23:21:02 +0530

    shadcn #16

--- src/app/(services)/planer/page.js
+++ src/app/(services)/planer/page.js
@@ -1,4 +1,4 @@
-import * as React from "react"
+import { Loader2 } from "lucide-react"
 
 import { Card, CardContent } from "@/components/ui/card"
 import {
@@ -8,33 +8,14 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import { Button } from "@/components/ui/button"
 
 export default function Planer() {
   return (
     <>
-    <div className="texts flex w-full justify-center justify-center h-40 border">
-      <Carousel
-      opts={{
-        align: "center",
-      }}
-      className="w-full max-w-sm h-full  self-center "
-    >
-      <CarouselContent className="w-full">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-3xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <div className="texts flex w-full align-center justify-center h-40 border">
+    <Button variant="ghost" disabled><Loader2 className="mr-2 h-4 w-4 animate-spin" />Button</Button>
+    
     </div>
 
 
