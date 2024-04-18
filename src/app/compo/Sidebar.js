7ddb6879b294fdc8671099a1b3c77acaa0a04327
sh20raj <shaswatraj3@gmail.com>
2024-04-19 00:31:04 +0530

    Chirp Introduced (text alternative) many shadcn ui added

--- src/app/compo/Sidebar.js
+++ src/app/compo/Sidebar.js
@@ -44,7 +44,7 @@ export default function Sidebar({active}) {
             <polygon points="10 8 16 12 10 16 10 8" />
           </svg>
         </Link>
-        <Link href={"/text"}  className={"app-sidebar-link " + (active === "text" ? "active" : "")}>
+        <Link href={"/Chirp"}  className={"app-sidebar-link " + (active === "text" ? "active" : "")}>
 <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-feather"><path d="M20.24 12.24a6 6 0 0 0-8.49-8.49L5 10.5V19h8.5z" /><line x1={16} y1={8} x2={2} y2={22} /><line x1="17.5" y1={15} x2={9} y2={15} /></svg>
         </Link>
         
--- src/components/ui/carousel.jsx
+++ src/components/ui/carousel.jsx
@@ -1,7 +1,7 @@
 "use client";
 import * as React from "react"
-import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons"
 import useEmblaCarousel from "embla-carousel-react";
+import { ArrowLeft, ArrowRight } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
@@ -163,7 +163,7 @@ const CarouselPrevious = React.forwardRef(({ className, variant = "outline", siz
       disabled={!canScrollPrev}
       onClick={scrollPrev}
       {...props}>
-      <ArrowLeftIcon className="h-4 w-4" />
+      <ArrowLeft className="h-4 w-4" />
       <span className="sr-only">Previous slide</span>
     </Button>)
   );
@@ -184,7 +184,7 @@ const CarouselNext = React.forwardRef(({ className, variant = "outline", size = 
       disabled={!canScrollNext}
       onClick={scrollNext}
       {...props}>
-      <ArrowRightIcon className="h-4 w-4" />
+      <ArrowRight className="h-4 w-4" />
       <span className="sr-only">Next slide</span>
     </Button>)
   );
