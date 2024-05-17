f73da80415d95fe2089fcdc248a158bd7beeb1ac
sh20raj <shaswatraj3@gmail.com>
2024-05-17 18:03:06 +0530

    key error fixed

--- src/app/compo/Articles.js
+++ src/app/compo/Articles.js
@@ -1,9 +1,9 @@
-'use client'
+"use client";
+
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import Skeleton from 'react-loading-skeleton';  // Import the Skeleton component
 import 'react-loading-skeleton/dist/skeleton.css'
-
 
 function Articles() {
   const [articles, setArticles] = useState([]);
@@ -26,8 +26,8 @@ function Articles() {
   }, []);
 
   // Skeleton layout
-  const RenderSkeleton = ({key}) => (
-    <div key={key} className="message-box">
+  const RenderSkeleton = ({ itemKey }) => (
+    <div key={itemKey} className="message-box">
       <div className="message-content">
         <Skeleton width="70%" height={20} />
         <Skeleton width="90%" height={15} style={{ marginTop: '10px' }} />
@@ -46,10 +46,10 @@ function Articles() {
       </div>
       <div className="messages">
         {loading ? (
-          Array.from({ length: 15 }).map((_, key) => <RenderSkeleton key={key}/>)
+          Array.from({ length: 15 }).map((_, k) => <RenderSkeleton key={k} itemKey={k} />)
         ) : (
           articles.map((article, i) => (
-            <div className="message-box" key={article.id || i}>
+            <div className="message-box" key={i}>
               <div className="message-content">
                 <div className="message-header">
                   <Link href={`/article/${article.id}`}>
