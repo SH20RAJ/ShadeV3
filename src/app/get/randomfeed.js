b8c6ca4a2e12b922b10b7e830973a7f03c595243
sh20raj <shaswatraj3@gmail.com>
2024-05-18 08:52:51 +0530

    #21 Random Feed API with RawSQL and Posts with type filter and User Details

--- src/app/get/randomfeed.js
+++ src/app/get/randomfeed.js
@@ -1,15 +1,58 @@
-import prisma from "../../../prisma"
+import prisma from "../../../prisma";
 
-let randomfeed = async ({type,limit}) => {
+let randomfeed = async ({
+  type,
+  limit = 20,
+  skip = 0,
+  orderBy = "Post.createdAt",
+}) => {
+  let filter = "";
+  if (type) {
+    filter = "WHERE Post.type = ?";
+  }
 
-    let filter = ""
-    if(type) {filter = 'where type = "' + type+'"'}
+  let orderClause = orderBy ? `ORDER BY ${orderBy}` : "ORDER BY Post.createdAt";
 
+  const results = await prisma.$queryRawUnsafe(
+    `SELECT 
+    Post.id,
+    Post.content,
+    Post.taglist,
+    Post.image,
+    Post.contentURL,
+    Post.title,
+    Post.type,
+    Post.createdAt ,
+    Post.updatedAt ,
+    Post.status,
+    JSON_OBJECT(
+        'id', User.id,
+        'username', User.username,
+        'email', User.email,
+        'name', User.name,
+        'bio', User.bio,
+        'avatar', User.avatar,
+        'coverPhoto', User.coverPhoto,
+        'createdAt', User.createdAt,
+        'updatedAt', User.updatedAt,
+        'status', User.status
+    ) AS user
+FROM 
+    Post
+JOIN 
+    User ON Post.authorId = User.id
+WHERE 
+    Post.type = ${"'"+type+"'"} OR 'video'
+ORDER BY 
+    RAND();
+`
+    // type ? [type, limit, skip] : [limit, skip]
+  );
 
-  let results = await prisma.$queryRawUnsafe(
-    `SELECT * FROM Post ${filter} ORDER BY RAND() LIMIT ${limit || 20};`, // For MySQL and MariaDB
-  )
-  return results
-}
+  return results.map((post) => ({
+    ...post,
+    user: JSON.parse(post.user), // parse the JSON string into an object
+  }));
+};
 
-export default randomfeed
+export default randomfeed;
