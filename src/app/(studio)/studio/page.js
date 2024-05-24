cf70eee8084da59384d223d89a6f161548443715
sh20raj <shaswatraj3@gmail.com>
2024-05-24 20:00:32 +0530

    catching disabled

--- src/app/(studio)/studio/page.js
+++ src/app/(studio)/studio/page.js
@@ -1,13 +1,20 @@
+import { headers } from "next/headers";
 import { StudioPage } from "./StudioPage";
 
 
-
-
 export const metadata = {
-    title : "Studio | Shade"
+    title : "Studio || Shade"
 }
 // app/studio/page.js
 export default function page() {
+
+    // Get headers object
+    const headersInstance = headers();
+  
+    // Set cache control headers
+    headersInstance.set('Cache-Control', 'no-store, max-age=0');
+
+    
     return (
       <>
       <StudioPage/>
